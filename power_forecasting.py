0582fd7ebc54e4fcd9e857c2c779540e8d2026d1
slowis <shaunlowis@gmail.com>
2024-10-28 11:45:02 +1300

    switching to ubuntu for development.

---
+++ power_forecasting.py
@@ -0,0 +1,10 @@
+"""
+Written by J.M. (Shaun) Lowis, for ENME618: Future Project Assignment.
+
+Data for this repository was retrieved from:
+https://www.mbie.govt.nz/building-and-energy/energy-and-natural-resources/energy-statistics-and-modelling/energy-statistics/electricity-statistics
+
+Please see the attached report for further information regarding model parameters.
+"""
+
+import pandas as pd
