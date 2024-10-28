13264703db2b81a1285bae880f1844de4ef6b611
slowis <shaunlowis@gmail.com>
2024-10-28 17:23:18 +1300

    initial guess at monte carlo

--- power_forecasting.py
+++ power_forecasting.py
@@ -7,6 +7,9 @@ https://www.mbie.govt.nz/building-and-energy/energy-and-natural-resources/energy
 Please see the attached report for further information regarding model parameters.
 
 In VS Code, you can go: Ctrl+SHIFT+P --> Select Interpreter --> + Create Virtual Environment for setup.
+
+You can then install the needed packages with: 
+python -m pip install -r requirements.txt
 """
 
 # File I/O
@@ -18,7 +21,6 @@ import numpy as np
 
 # Curve fitting
 import prophet
-from scipy.optimize import curve_fit
 
 # Plotting
 import matplotlib.pyplot as plt
@@ -69,6 +71,77 @@ def prophet_forecast(cleaned_df):
     fig.savefig("projection_historical.pdf")
 
 
+def monte_carlo_growth(cleaned_df):
+    # use our historical data
+    base_value = cleaned_df["y"].values[-1]
+    years = 26
+    num_simulations = 1000
+    # These should be taken from the dataset maybe
+    target_growth_rate = 0.02  # 5% target annual growth rate
+    std_growth_rate = 0.02  # get std from data.
+    predictions = []
+
+    # This is a very basic approach, but good enough for our case.
+    for _ in range(num_simulations):
+        future_values = [base_value]
+
+        # We iterate through the number of years for each simulatio
+        # so we can generate a large enough sample size to conformt to the central limit theorem.
+        for __ in range(years):
+            sampled_growth_rate = np.random.normal(
+                loc=target_growth_rate, scale=std_growth_rate
+            )
+            # growth projection comes in here.
+            next_value = future_values[-1] * (1 + sampled_growth_rate)
+            future_values.append(next_value)
+
+        predictions.append(future_values)
+
+    return pd.DataFrame(predictions)
+
+
+def plot_monte_carlo(df):
+
+    # Analyze results
+    mean_prediction = df.mean(axis=0)
+    lower_bound = df.quantile(0.025, axis=0)
+    upper_bound = df.quantile(0.975, axis=0)
+
+    # Styling so output looks nice
+    import matplotlib_rc
+
+    fig, ax = plt.subplots()
+
+    ax.plot(mean_prediction, label="Mean Prediction", color="blue")
+    ax.fill_between(
+        range(26 + 1),
+        lower_bound,
+        upper_bound,
+        color="blue",
+        alpha=0.2,
+        label="95% Confidence Interval",
+    )
+
+    ax.hlines(
+        y=72000,
+        xmin=0,
+        xmax=26,
+        linewidth=0.5,
+        linestyle="--",
+        label="Upper limit for power generation",
+        color="r",
+    )
+
+    ax.set_title("Projected Future Values with Positive Growth Scenario")
+    ax.set_xlabel("Years")
+    ax.set_ylabel("Projected Value")
+    ax.legend()
+    ax.grid()
+
+    # plt.show()
+    plt.savefig("first_monte_carlo.pdf")
+
+
 def main():
     fp = os.path.join(os.getcwd(), "electricity.xlsx")
     # Retrieve annual power generation:
@@ -84,7 +157,9 @@ def main():
         }
     )
 
-    prophet_forecast(reduced_df)
+    # prophet_forecast(reduced_df)
+    df_carlo = monte_carlo_growth(reduced_df)
+    plot_monte_carlo(df_carlo)
 
 
 if __name__ == main():
